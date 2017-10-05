40c848049328a9ad20883b78631f57383fa62549
Resi Respati <resi.respati@compare88.net>
2017-10-05 15:41:31 +0700

    404.

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 const NotFoundPage = () => (
   <div>
-    <h1>NOT FOUND</h1>
-    <p>You just hit a page that doesn&#39;t exist... the sadness.</p>
+    <h1>404: NOT FOUND</h1>
+    <p><img src="/img/tenshi.gif" /></p>
   </div>
 )
 
