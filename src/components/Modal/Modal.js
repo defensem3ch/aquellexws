d6f3213955578fe59e6095934bcff81cdc3096b4
Resi Respati <resi.respati@compare88.net>
2017-10-09 18:20:16 +0700

    add initial modal support

---
+++ src/components/Modal/Modal.js
@@ -0,0 +1,21 @@
+import * as React from 'react'
+import PropTypes from 'prop-types'
+import ReactModal from 'react-modal'
+
+class Modal extends React.Component {
+  static propTypes = {
+    isOpen: PropTypes.bool,
+    label: PropTypes.string.isRequired,
+    children: PropTypes.node.isRequired
+  }
+
+  render () {
+    const { isOpen, label, children } = this.props
+
+    return (
+      <ReactModal contentLabel={label}>
+        {children}
+      </ReactModal>
+    )
+  }
+}
