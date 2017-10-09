a598c099c8ddc11cd35223e369f96deae4f4477d
Aquellex <gammablaster93@hotmail.com>
2017-10-09 15:44:19 +1100

    removed unused yzyx gif, changed favicon to yomiell

--- src/html.js
+++ src/html.js
@@ -29,7 +29,7 @@ module.exports = class HTML extends React.Component {
             name="viewport"
             content="width=device-width, initial-scale=1, shrink-to-fit=no"
           />
-          <link rel="shortcut icon" href={process.env.__PATH_PREFIX__ || '' + '/img/fxsql.png'} />
+          <link rel="shortcut icon" href={process.env.__PATH_PREFIX__ || '' + '/img/yomiell.png'} />
           {this.props.headComponents}
           {css}
         </head>
