6d6079a0bcdc863ce8442f0d4a735f256b41a908
Resi Respati <resi.respati@compare88.net>
2017-11-01 12:10:24 +0700

    re-use Promise instead of async/await

--- gatsby-node.js
+++ gatsby-node.js
@@ -67,82 +67,84 @@ exports.onCreateNode = ({ node, getNode, boundActionCreators }) => {
   }
 }
 
-exports.createPages = async ({ graphql, boundActionCreators }) => {
+exports.createPages = ({ graphql, boundActionCreators }) => {
   const { createPage, createRedirect } = boundActionCreators
 
   // Used to detect and prevent duplicate redirects
   const redirectToSlugMap = {}
 
-  const allMarkdown = await graphql(
-    `
-      {
-        allMarkdownRemark(limit: 1000) {
-          edges {
-            node {
-              fields {
-                redirect
-                slug
-                layout
+  return new Promise((resolve, reject) => {
+    graphql(
+      `
+        {
+          allMarkdownRemark(limit: 1000) {
+            edges {
+              node {
+                fields {
+                  redirect
+                  slug
+                  layout
+                }
               }
             }
           }
         }
-      }
-    `
-  )
-
-  if (allMarkdown.errors) {
-    console.error (allMarkdown.errors)
-    throw new Error(allMarkdown.errors)
-  }
-
-  allMarkdown.data.allMarkdownRemark.edges.forEach(({ node }) => {
-    const { slug, layout, redirect } = node.fields
-
-    createPage({
-      path: slug,
-      // Feel free to set any `layout` as you'd like in the frontmatter, as
-      // long as the corresponding template file exists in src/templates.
-      // If no template is set, it will fall back to the default `page`
-      // template. (Hardcoded for `.js` file extension!)
-      component: path.resolve(`./src/templates/${layout || 'page'}.js`),
-      context: {
-        // Data passed to context is available in page queries as GraphQL variables.
-        slug
-      }
-    })
-
-    // URL redirect handler
-    // Adapted from reactjs/reactjs.org:
-    // https://github.com/reactjs/reactjs.org/blob/master/gatsby-node.js#L111
-    if (redirect) {
-      let toRedirect = JSON.parse(node.fields.redirect)
-      if (!Array.isArray(toRedirect)) {
-        toRedirect = [toRedirect]
+      `
+    ).then(result => {
+      if (result.errors) {
+        reject(result.errors)
       }
 
-      toRedirect.forEach(fromPath => {
-        if (redirectToSlugMap[fromPath] != null) {
-          console.error(
-            `Duplicate redirect detected from "${fromPath}" to:\n` +
-              `* ${redirectToSlugMap[fromPath]}\n` +
-              `* ${slug}\n`,
-          )
-          process.exit(1)
-        }
+      result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+        const { slug, layout, redirect } = node.fields
 
-        // A leading "/" is required for redirects to work,
-        // But multiple leading "/" will break redirects.
-        // For more context see github.com/reactjs/reactjs.org/pull/194
-        const toPath = slug.startsWith('/') ? slug : `/${slug}`
-
-        redirectToSlugMap[fromPath] = slug
-        createRedirect({
-          fromPath: `/${fromPath}`,
-          redirectInBrowser: true,
-          toPath
+        createPage({
+          path: slug,
+          // Feel free to set any `layout` as you'd like in the frontmatter, as
+          // long as the corresponding template file exists in src/templates.
+          // If no template is set, it will fall back to the default `page`
+          // template. (Hardcoded for `.js` file extension!)
+          component: path.resolve(`./src/templates/${layout || 'page'}.js`),
+          context: {
+            // Data passed to context is available in page queries as GraphQL variables.
+            slug
+          }
         })
+
+        // URL redirect handler
+        // Adapted from reactjs/reactjs.org:
+        // https://github.com/reactjs/reactjs.org/blob/master/gatsby-node.js#L111
+        if (redirect) {
+          let toRedirect = JSON.parse(node.fields.redirect)
+          if (!Array.isArray(toRedirect)) {
+            toRedirect = [toRedirect]
+          }
+
+          toRedirect.forEach(fromPath => {
+            if (redirectToSlugMap[fromPath] != null) {
+              console.error(
+                `Duplicate redirect detected from "${fromPath}" to:\n` +
+                  `* ${redirectToSlugMap[fromPath]}\n` +
+                  `* ${slug}\n`,
+              )
+              process.exit(1)
+            }
+
+            // A leading "/" is required for redirects to work,
+            // But multiple leading "/" will break redirects.
+            // For more context see github.com/reactjs/reactjs.org/pull/194
+            const toPath = slug.startsWith('/') ? slug : `/${slug}`
+
+            redirectToSlugMap[fromPath] = slug
+            createRedirect({
+              fromPath: `/${fromPath}`,
+              redirectInBrowser: true,
+              toPath
+            })
+          })
+        }
       })
-    }
+      resolve()
+    })
   })
 }
