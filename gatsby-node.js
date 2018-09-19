7bfd3e3a14e63cde75d439614310d6d987b9e768
Resi Respati <resir014@gmail.com>
2018-09-20 02:56:44 +0700

    implement eslint + prettier

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,75 +1,79 @@
 // @ts-check
 
-'use strict'
+'use strict';
 
-const path = require('path')
-const slugify = require('slug')
-const { createFilePath } = require('gatsby-source-filesystem')
+const path = require('path');
+const slugify = require('slug');
+const { createFilePath } = require('gatsby-source-filesystem');
 
 // Regex to parse date and title from the filename
-const BLOG_POST_SLUG_REGEX = /^\/blog\/([\d]{4})-([\d]{2})-([\d]{2})-(.+)\/$/
+const BLOG_POST_SLUG_REGEX = /^\/blog\/([\d]{4})-([\d]{2})-([\d]{2})-(.+)\/$/;
 
 exports.onCreateNode = ({ node, getNode, actions }) => {
-  const { createNodeField } = actions
+  const { createNodeField } = actions;
 
   if (node.internal.type === `MarkdownRemark`) {
-    const { permalink, redirect_from, category, layout, lastUpdated } = node.frontmatter
-    const relativePath = createFilePath({ node, getNode, basePath: 'pages' })
+    const { permalink, redirect_from, category, layout, lastUpdated } = node.frontmatter;
+    const relativePath = createFilePath({ node, getNode, basePath: 'pages' });
 
     let slug = permalink;
 
     if (!slug && relativePath.includes('blog')) {
       // Generate final path + graphql fields for blog posts
-      const match = BLOG_POST_SLUG_REGEX.exec(relativePath)
-      const year = match[1]
-      const month = match[2]
-      const day = match[3]
-      const filename = match[4]
+      const match = BLOG_POST_SLUG_REGEX.exec(relativePath);
+      const year = match[1];
+      const month = match[2];
+      const day = match[3];
+      const filename = match[4];
 
-      slug = `/blog/${year}/${month}/${day}/${slugify(filename)}/`
+      slug = `/blog/${year}/${month}/${day}/${slugify(filename)}/`;
 
-      const date = new Date(Number.parseInt(year), Number.parseInt(month) - 1, Number.parseInt(day))
+      const date = new Date(
+        Number.parseInt(year),
+        Number.parseInt(month) - 1,
+        Number.parseInt(day)
+      );
 
       // Blog posts are sorted by date and display the date in their header.
       createNodeField({
         node,
         name: 'date',
-        value: date.toJSON()
-      })
+        value: date.toJSON(),
+      });
     }
 
     if (!slug) {
-      slug = relativePath
+      slug = relativePath;
     }
 
     // Used to generate URL to view this content.
     createNodeField({
       node,
       name: `slug`,
-      value: slug
-    })
+      value: slug,
+    });
 
     // Used to determine a page layout.
     createNodeField({
       node,
       name: 'layout',
       value: layout || '',
-    })
+    });
 
     // Used by createPages() to register redirects.
     createNodeField({
       node,
       name: 'redirect',
       value: redirect_from ? JSON.stringify(redirect_from) : '',
-    })
+    });
   }
-}
+};
 
 exports.createPages = ({ graphql, actions }) => {
-  const { createPage, createRedirect } = actions
+  const { createPage, createRedirect } = actions;
 
   // Used to detect and prevent duplicate redirects
-  const redirectToSlugMap = {}
+  const redirectToSlugMap = {};
 
   return new Promise((resolve, reject) => {
     graphql(
@@ -90,11 +94,11 @@ exports.createPages = ({ graphql, actions }) => {
       `
     ).then(result => {
       if (result.errors) {
-        reject(result.errors)
+        reject(result.errors);
       }
 
       result.data.allMarkdownRemark.edges.forEach(({ node }) => {
-        const { slug, layout, redirect } = node.fields
+        const { slug, layout, redirect } = node.fields;
 
         createPage({
           path: slug,
@@ -105,17 +109,17 @@ exports.createPages = ({ graphql, actions }) => {
           component: path.resolve(`./src/templates/${layout || 'page'}.js`),
           context: {
             // Data passed to context is available in page queries as GraphQL variables.
-            slug
-          }
-        })
+            slug,
+          },
+        });
 
         // URL redirect handler
         // Adapted from reactjs/reactjs.org:
         // https://github.com/reactjs/reactjs.org/blob/master/gatsby-node.js#L111
         if (redirect) {
-          let toRedirect = JSON.parse(node.fields.redirect)
+          let toRedirect = JSON.parse(node.fields.redirect);
           if (!Array.isArray(toRedirect)) {
-            toRedirect = [toRedirect]
+            toRedirect = [toRedirect];
           }
 
           toRedirect.forEach(fromPath => {
@@ -124,24 +128,24 @@ exports.createPages = ({ graphql, actions }) => {
                 `Duplicate redirect detected from "${fromPath}" to:\n` +
                   `* ${redirectToSlugMap[fromPath]}\n` +
                   `* ${slug}\n`
-              )
+              );
             }
 
             // A leading "/" is required for redirects to work,
             // But multiple leading "/" will break redirects.
             // For more context see github.com/reactjs/reactjs.org/pull/194
-            const toPath = slug.startsWith('/') ? slug : `/${slug}`
+            const toPath = slug.startsWith('/') ? slug : `/${slug}`;
 
-            redirectToSlugMap[fromPath] = slug
+            redirectToSlugMap[fromPath] = slug;
             createRedirect({
               fromPath: `/${fromPath}`,
               redirectInBrowser: true,
-              toPath
-            })
-          })
+              toPath,
+            });
+          });
         }
-      })
-      resolve()
-    })
-  })
-}
+      });
+      resolve();
+    });
+  });
+};
